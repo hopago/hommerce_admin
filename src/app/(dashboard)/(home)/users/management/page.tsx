d66f8a6fe719e1b4a4beddcfdf5e5e44ac5f9e03
hopago <132269386+hopago@users.noreply.github.com>
2024-03-31 13:01:18 +0900

    관리 페이지 윈도우 분기 처리

--- src/app/(dashboard)/(home)/users/management/page.tsx
+++ src/app/(dashboard)/(home)/users/management/page.tsx
@@ -7,17 +7,18 @@ import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 export default function UserManagement() {
-  const { usernames } = useManageUsers();
+  if (typeof window !== "undefined") {
+    const { usernames } = useManageUsers();
 
-  const router = useRouter();
+    const router = useRouter();
 
-  if (!usernames || !usernames.length) {
-    toast.message("선택된 유저가 없습니다.");
-    router.back();
-    return null;
+    if (!usernames || !usernames.length) {
+      toast.message("선택된 유저가 없습니다.");
+      router.back();
+    }
+
+    router.push(`/users/management/${usernames![0]}`);
   }
-
-  router.push(`/users/management/${usernames![0]}`);
 
   return null;
 }
